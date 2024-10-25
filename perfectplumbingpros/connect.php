d991ee32148780aabb42e229086850cc963a77aa
Juno Dela Cruz <55854673+Anetryst@users.noreply.github.com>
2024-10-25 16:16:12 +0000

    change connect.php

--- perfectplumbingpros/connect.php
+++ perfectplumbingpros/connect.php
@@ -1,9 +1,16 @@
 <?php
-   
-   $conn = mysqli_connect("sql1.njit.edu", "jtd43", "h1aYWpeF_0124!", "jtd43"); 
-    if(mysqli_connect_errno())
+    //Makes DB connection
+    $servername = "sql1.njit.edu";
+    $username = "jtd43";
+    $password = "h1aYWpeF_0124!";
+    $dbname = "jtd43";
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+    if (mysqli_connect_errno())
     {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-
+    else
+    {
+    echo "CONNECTED ";
+    }
 ?>
