765fe53e14ad2d90e9d2901bcb0056dbf2c6cdab
Juno Dela Cruz <55854673+Anetryst@users.noreply.github.com>
2024-10-25 19:02:25 -0400

    Update connect.php

--- perfectplumbingpros/connect.php
+++ perfectplumbingpros/connect.php
@@ -4,7 +4,7 @@
     $username = "jtd43";
     $password = "h1aYWpeF_0124!";
     $dbname = "jtd43";
-    $con = mysqli_connect($servername,$username,$password,$dbname);
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
     if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
